e64ba4ccf8a1d6a672c2ef257f54727ec1f80ae9
DKDK <kinetik-ninja@web.de>
2018-12-08 15:40:49 +0100

    git remote add origin https://github.com/user/repo.git

--- VirtualBoxSnapShot.sh
+++ VirtualBoxSnapShot.sh
@@ -5,4 +5,15 @@ SNAPSHOT_DESCRIPTION="Snapshot taken on $NOW"
 
 VBoxManage snapshot UBUNTU18 take "$SNAPSHOT_NAME" --description "$SNAPSHOT_DESCRIPTION"
 
-#Simple and funny
+
+#git remote add origin https://github.com/user/repo.git
+# Set a new remote
+#git remote -v
+# Verify new remote
+
+#origin  https://github.com/user/repo.git (fetch)
+#origin  https://github.com/user/repo.git (push)
+
+#git remote add origin https://github.com/DKDKDKDKDKDKDKDK/VBoxManage.git
+
+#git push
